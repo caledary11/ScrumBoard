408fce63bbd1e2ebc8902fed17def23bc16ad794
Ken <ken.woo@ennovatelabs.com>
2015-02-10 22:36:42 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScrumBoard::Application.config.secret_key_base = '9a35a55dfcb857893c0cefa0a6852fff793ca995a1baa0ac7d2ce4c161134cbd7bff9655e54009c93531cadd6249e0eb927dfe2c1de86afa65f9cd5e526e182f'
